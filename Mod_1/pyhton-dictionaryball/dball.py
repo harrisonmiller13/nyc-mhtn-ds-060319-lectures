4529b95ae4bec57472431d3d65b20db01b3ae1b2
“Harrison <harrison.miller13@gmail.com>
2019-06-07 14:00:21 -0400

    updated proj

--- Mod_1/pyhton-dictionaryball/dball.py
+++ Mod_1/pyhton-dictionaryball/dball.py
@@ -3,11 +3,12 @@ from dictionaryball import game_dictionary
 def game_dict():
     return game_dictionary
 
-# homeplayer = game_dict()['home']['players']
-# awayplayer = game_dict()['away']['players']
-# all_players = {**homeplayer,**awayplayer}
-# homename = game_dict()['home']['team_name']
-# awayname = game_dict()['away']['team_name']
+# def dict_var():
+#     homeplayer = game_dict()['home']['players']
+#     awayplayer = game_dict()['away']['players']
+#     all_players = {**homeplayer,**awayplayer}
+#     homename = game_dict()['home']['team_name']
+#     awayname = game_dict()['away']['team_name']
 
 def num_points_scored (player):
     homeplayer = game_dict()['home']['players']
@@ -27,10 +28,31 @@ def team_colors (team_name):
     if team_name == homename:
         return game_dict()['home']['colors']
     return game_dict()['away']['colors']
+        
 
-def whos_playing ():
+def team_names ():
     homename = game_dict()['home']['team_name']
     awayname = game_dict()['away']['team_name']
-    return homename + ' ' + 'vs' + ' ' + awayname
+    return [homename, awayname]
+
+def player_stats (player):
+    homeplayer = game_dict()['home']['players']
+    awayplayer = game_dict()['away']['players']
+    all_players = {**homeplayer,**awayplayer}
+    return all_players[player]
+
+def player_numbers (team):
+    home = game_dict()['home']
+    homeplayer = game_dict()['home']['players']
+    awayplayer = game_dict()['away']['players']
+    jersey_numbers = []
+    if team == home['team_name']:
+        for player, stats in homeplayer.items():
+            jersey_numbers.append (stats ['number'])
+    else:
+        for player, stats in awayplayer.items():
+            jersey_numbers.append (stats['number'])
+    return jersey_numbers
+
 
 
